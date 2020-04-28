7e0a24d97c1ca351960f6d9131716e17436a20d9
Julio Panjaitan <panjaitanjulio22@gmail.com>
2020-04-28 23:14:34 +0700

    form

--- project-tarif/resources/js/components/FormPage.vue
+++ project-tarif/resources/js/components/FormPage.vue
@@ -1,21 +1,53 @@
 <template>
-    <div>
-        Pilih Jenis Barang
-        <b-form-select v-model="id_kategori" :options="kategori" v-bind:value="kategori.id"></b-form-select>
-        <model-list-select :list="tujuan_makanan_non"
-                           v-model="id_tujuan_makanan_non"
-                           option-value="id"
-                           option-text="nama_tempat"
-                           placeholder="Pilih Tujuan">
-        </model-list-select>
-    </div>
+    <b-container>
+        <b-form-row>
+            <h3>Pilih Jenis Paket</h3>
+            <b-form-select
+                v-model="id_kategori"
+                :options="kategori"
+                value-field="id"
+                text-field="kategori">
+            </b-form-select>
 
+            <h3>Pilih Asal Pengiriman</h3>
+            <b-form-select
+                v-model="id_asal"
+                :options="asal_pengiriman"
+                value-field="id"
+                text-field="nama_tempat">
+            </b-form-select>
+
+            <h3>Pilih Tujuan Pengiriman</h3>
+            <model-list-select :list="tujuan_makanan_non"
+                               v-model="id_tujuan_makanan_non"
+                               option-value="id"
+                               option-text="nama_tempat"
+                               placeholder="Pilih Tujuan">
+            </model-list-select>
+
+            <h3>Masukan Berat Paket</h3>
+            <b-form-input
+                id="berat-paket"
+                v-model="berat_paket"
+                type="text"
+                required>
+            </b-form-input>
+        </b-form-row>
+
+
+            {{ id_kategori }}
+            {{ id_tujuan_makanan_non }}
+    </b-container>
 </template>
 
 <script>
     import axios from 'axios'
+    import { ModelListSelect } from 'vue-search-select';
     export default {
         name: "FormPage",
+        components: {
+            ModelListSelect
+        },
         data(){
             return{
                 kategori: [],
@@ -27,7 +59,8 @@
                 id_kategori: 0,
                 id_asal: 0,
                 id_tujuan_dokumen: 0,
-                id_tujuan_makanan_non: 0
+                id_tujuan_makanan_non: 0,
+                berat_paket: 0
             }
         },
         async mounted() {
---
+++ project-tarif/resources/views/form.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <title>Cek Tarif Pengiriman</title>
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+</head>
+
+<body class="content">
+
+    <div id="app">
+
+        <form-page></form-page>
+
+    </div>
+
+</body>
+</html>
