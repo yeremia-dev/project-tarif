52677fbe625a2fa9b5bc9fd05fa208320833cb5e
Julio Panjaitan <panjaitanjulio22@gmail.com>
2020-04-28 22:31:38 +0700

    work 2

--- project-tarif/resources/views/welcome.blade.php
+++ project-tarif/resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
 
         <title>Laravel</title>
 
-        <!-- Fonts -->
+        <script src="{{ asset('js/app.js') }}"></script>
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
         <!-- Styles -->
@@ -84,15 +84,8 @@
                     Laravel
                 </div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                <div class="app">
+                    <example-component></example-component>
                 </div>
             </div>
         </div>
